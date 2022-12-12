6864ae54e4568eaf9546c911c197a1f6d3c3aaed
BlearyButton <117636640+BlearyButton@users.noreply.github.com>
2022-12-12 21:35:27 +0100

    tailwind setup

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'] 
+      },
+      colors: {
+        'darkbeige': '#453b27',
+        'borderbeige': '#453b27',
+      },
+    }
+  },
+  plugins: [],
+}
